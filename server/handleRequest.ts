46624aabc50c0f48a0344c45c1a0c3df0fe64bc6
Matt Hayes <matt@mysterycommand.com>
2025-04-14 20:10:42 -0500

    refactor: make it stupidly idiomatic ... I already like it more

--- server/handleRequest.ts
+++ server/handleRequest.ts
@@ -6,19 +6,19 @@ type F<T> = T extends new (...args: infer A) => infer R
   ? (...args: A) => R
   : never;
 
-const response: F<typeof Response> = (body, init) => new Response(body, init);
-const statusResponse = (code: StatusCode) =>
-  response(`${code} ${STATUS_TEXT[code]}`, {
+const fres: F<typeof Response> = (body, init) => new Response(body, init);
+const stat = (code: StatusCode) =>
+  fres(`${code} ${STATUS_TEXT[code]}`, {
     status: code,
     statusText: STATUS_TEXT[code],
   });
 
-const url: F<typeof URL> = (url, base) => new URL(url, base);
+const furl: F<typeof URL> = (url, base) => new URL(url, base);
 const walk = async function* (
   dir: string,
   base: string
 ): AsyncGenerator<string> {
-  for await (const entry of Deno.readDir(url(dir, base))) {
+  for await (const entry of Deno.readDir(furl(dir, base))) {
     if (entry.isDirectory) {
       yield* walk(join(dir, entry.name), base);
     }
@@ -36,10 +36,10 @@ const MIME_TYPE = {
   [".js"]: "text/javascript",
 } as const;
 
-const readable = (path: URL) =>
+const rope = (path: URL) =>
   Deno.open(path, { read: true }).then(({ readable }) => readable);
 
-const compiled = (path: URL) =>
+const rile = (path: URL) =>
   Deno.readTextFile(path)
     .then((contents) => transform(contents))
     /**
@@ -49,10 +49,10 @@ const compiled = (path: URL) =>
     .then(({ code }) => code.replace(/"(\..*)\.ts"/g, '"$1.js"'));
 
 export const handleRequest = async (req: Request): Promise<Response> => {
-  const parsed = parse(decodeURIComponent(url(req.url).pathname));
+  const parsed = parse(decodeURIComponent(furl(req.url).pathname));
   let { dir, ext, name } = parsed;
 
-  if (dir !== "/") return statusResponse(403);
+  if (dir !== "/") return stat(403);
 
   if (ext === ".js") {
     ext = ".ts";
@@ -72,23 +72,23 @@ export const handleRequest = async (req: Request): Promise<Response> => {
   });
 
   if (!files.includes(path)) {
-    return statusResponse(404);
+    return stat(404);
   }
 
   switch (ext) {
     case ".css":
     case ".html": {
-      return response(await readable(url(path, Deno.mainModule)), {
+      return fres(await rope(furl(path, Deno.mainModule)), {
         headers: { ["Content-Type"]: MIME_TYPE[ext] },
       });
     }
 
     case ".ts": {
-      return response(await compiled(url(path, Deno.mainModule)), {
+      return fres(await rile(furl(path, Deno.mainModule)), {
         headers: { ["Content-Type"]: MIME_TYPE[".js"] },
       });
     }
   }
 
-  return statusResponse(501);
+  return stat(501);
 };
