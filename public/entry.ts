507028d91ad75328337241e885be44f42a3b8bc6
Matt Hayes <matt@mysterycommand.com>
2025-04-14 23:29:59 -0500

    spike: try parsing the current dom state into an object

--- public/entry.ts
+++ public/entry.ts
@@ -1,2 +1,46 @@
+declare const m: HTMLElement;
+
+type Tree = {
+  thing: string;
+  stuff?: {
+    [k: string]: unknown;
+    babes?: (Tree | string)[];
+  };
+};
+
+const peer = (as: NamedNodeMap): { [k: string]: unknown } => {
+  const amap: { [k: string]: unknown } = {};
+
+  for (const a of as) {
+    amap[a.name] = a.value;
+  }
+
+  return amap;
+};
+
+const look = (el: Element): Tree => {
+  const thing = el.tagName.toLowerCase(),
+    stuff = el.hasAttributes() && peer(el.attributes),
+    babes = el.hasChildNodes()
+      ? Object.values(el.childNodes).map<Tree | string>((node) =>
+          node.nodeName === "#text"
+            ? (node as Text).data
+            : look(node as Element)
+        )
+      : undefined;
+
+  return {
+    thing: thing,
+    ...((el.hasAttributes() || el.hasChildNodes()) && {
+      stuff: {
+        ...stuff,
+        babes,
+      },
+    }),
+  };
+};
+
+console.log(JSON.stringify(look(m), null, 2));
+
 const worky = new Worker("./worky.ts", { type: "module" });
 worky.addEventListener("message", ({ data }) => console.log(data));
