095518073894f1f0027576d76e18605c854e4ee0
Matt Hayes <matt@mysterycommand.com>
2025-04-16 13:05:55 -0500

    feat: scan an existing dom to build up a 'virtual dom'

--- public/entry.ts
+++ public/entry.ts
@@ -1,45 +1,49 @@
 declare const m: HTMLElement;
 
-type Tree = {
-  thing: string;
-  stuff?: {
-    [k: string]: unknown;
-    babes?: (Tree | string)[];
-  };
+type Rndr = keyof (HTMLElementTagNameMap | SVGElementTagNameMap);
+type Thng = {
+  rndr: Rndr;
+  data?: { [k: string]: unknown };
+  kids?: Kids;
 };
 
-const peer = (as: NamedNodeMap): { [k: string]: unknown } => {
-  const amap: { [k: string]: unknown } = {};
+type Kids = (Thng | string)[];
 
-  for (const a of as) {
-    amap[a.name] = a.value;
+const { from } = Array,
+  { assign, fromEntries } = Object;
+
+const peek = (as: NamedNodeMap): { [k: string]: unknown } =>
+  fromEntries(from(as, ({ name, value }) => [name, value]));
+
+const kids = (acc: Kids, node: ChildNode): Kids => {
+  switch (node.nodeType) {
+    case Node.ELEMENT_NODE: {
+      acc.push(scan(node as Element));
+      break;
+    }
+
+    case Node.TEXT_NODE: {
+      const { data } = node as Text;
+      // only include nodes that *are not* a newline followed by all whitespace
+      if (!/^\n\s*$/.test(data)) {
+        acc.push(data);
+      }
+      break;
+    }
   }
 
-  return amap;
+  return acc;
 };
 
-const look = (el: Element): Tree => {
-  const thing = el.tagName.toLowerCase(),
-    stuff = el.hasAttributes() && peer(el.attributes),
-    babes = el.hasChildNodes()
-      ? Object.values(el.childNodes).map<Tree | string>((node) =>
-          node.nodeName === "#text"
-            ? (node as Text).data
-            : look(node as Element)
-        )
-      : undefined;
-
-  return {
-    thing: thing,
-    ...((el.hasAttributes() || el.hasChildNodes()) && {
-      stuff: {
-        ...stuff,
-        babes,
-      },
-    }),
-  };
-};
+const scan = ({ tagName, attributes, childNodes }: Element): Thng =>
+  assign(
+    { rndr: tagName.toLowerCase() as Rndr },
+    attributes.length && { data: peek(attributes) },
+    childNodes.length && {
+      kids: childNodes.values().reduce(kids, []),
+    }
+  );
 
 const worky = new Worker("./worky.ts", { type: "module" });
 worky.addEventListener("message", ({ data }) => console.log(data));
-worky.postMessage(look(m));
+worky.postMessage(scan(m));
