4b7830d73c4833daba7c651702c8e76a047acde7
Matt Hayes <matt@mysterycommand.com>
2025-04-17 17:37:21 -0500

    feat: setup hreq to search for tsx files too, configure swc to insert the pragma, but then string replace it after transformation

--- server/hreq.ts
+++ server/hreq.ts
@@ -1,6 +1,6 @@
 import { STATUS_TEXT, type StatusCode } from "jsr:@std/http/status";
-import { extname, format, join, parse, type ParsedPath } from "jsr:@std/path";
-import { transform } from "https://esm.sh/@swc/wasm-typescript@1.11.21";
+import { format, join, normalize, parse } from "jsr:@std/path";
+import initSwc, { transform } from "https://esm.sh/@swc/wasm-web@1.11.21";
 
 type F<T> = T extends new (...args: infer A) => infer R ? (...args: A) => R
   : never;
@@ -38,21 +38,74 @@ const MIME_TYPE = {
 const rope = (path: URL) =>
   Deno.open(path, { read: true }).then(({ readable }) => readable);
 
+await initSwc();
 const rile = (path: URL) =>
   Deno.readTextFile(path)
-    .then((contents) => transform(contents))
+    .then((contents) =>
+      transform(contents, {
+        "jsc": {
+          "parser": {
+            "syntax": "typescript",
+            "tsx": true,
+          },
+          // @ts-expect-error these types are just not up to date
+          "target": "es2024",
+          "loose": false,
+          "minify": {
+            "compress": false,
+            "mangle": false,
+          },
+          "transform": {
+            "react": {
+              /**
+               * still need to "manually" `import { el } from "./lib/real.ts";
+               * @see https://github.com/swc-project/swc/issues/2663
+               */
+              "runtime": "automatic",
+              "importSource": "real",
+            },
+          },
+        },
+        "module": {
+          "type": "es6",
+        },
+        "minify": false,
+        "isModule": true,
+      })
+    )
     /**
-     * replace the trailing `ts` with `js` for any "double-quoted" string that
-     * starts with `.` and ends with `ts`
+     * replace the trailing `ts` or `tsx` with `js` for any "double-quoted"
+     * string that starts with `.` and ends with `ts` or `tsx`
      */
-    .then(({ code }) => code.replace(/"(\..*)\.ts"/g, '"$1.js"'));
+    .then(({ code }) =>
+      code
+        .replace(/"(\..*)\.tsx?"/g, '"$1.js"')
+        .replace(
+          'import { jsx as _jsx } from "real/jsx-runtime";',
+          'import { el } from "./lib/real.js";',
+        )
+        .replace(/_jsx/g, "el")
+    );
 
-const mapf = (url: string): [ParsedPath, string] => {
-  const parsed = parse(decodeURIComponent(furl(url).pathname));
+const frmt = (dir: string, name: string, ext: string) =>
+  normalize(format({
+    base: name + ext,
+    dir: join("public", dir),
+    ext,
+    name,
+    root: "/",
+  }));
+
+const mapf = (
+  url: string,
+): [dir: string, ext: string, path: string] => {
+  const parsed = parse(decodeURIComponent(furl(url).pathname)),
+    { dir } = parsed;
   let { ext, name } = parsed;
 
   if (ext === ".js") {
-    ext = ".ts";
+    // assume `tsx` if we don't match on `ts`
+    ext = fs.includes(frmt(dir, name, ".ts")) ? ".ts" : ".tsx";
   }
 
   if (name === "") {
@@ -60,23 +113,13 @@ const mapf = (url: string): [ParsedPath, string] => {
     name = "index";
   }
 
-  return [
-    parsed,
-    format({
-      base: name + ext,
-      dir: "public",
-      ext,
-      name,
-      root: "/",
-    }),
-  ];
+  return [dir, ext, frmt(dir, name, ext)];
 };
 
 export const hreq = async ({ url }: Request): Promise<Response> => {
-  const [{ dir }, path] = mapf(url),
-    ext = extname(path);
+  const [dir, ext, path] = mapf(url);
 
-  if (dir !== "/") return stat(403);
+  if (!dir.startsWith("/")) return stat(403);
   if (!fs.includes(path)) return stat(404);
 
   switch (ext) {
@@ -87,7 +130,8 @@ export const hreq = async ({ url }: Request): Promise<Response> => {
       });
     }
 
-    case ".ts": {
+    case ".ts":
+    case ".tsx": {
       return fres(await rile(furl(path, Deno.mainModule)), {
         headers: { ["Content-Type"]: MIME_TYPE[".js"] },
       });
