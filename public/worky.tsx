f3343c5a47f0bbd4f3ebb1ce28e8bb7dd8eb08dc
Matt Hayes <matt@mysterycommand.com>
2025-04-19 07:14:13 -0500

    refactor: move the jsx to the worker

---
+++ public/worky.tsx
@@ -0,0 +1,26 @@
+self.postMessage(
+  <main id="m">
+    <hr />
+    <article className="the-article">
+      <header>
+        <h1>The Title</h1>
+        <h2>A Subheading</h2>
+      </header>
+      <section>
+        <p>Some content.</p>
+        <p>
+          Some <b>more</b>
+          <i>content</i>.
+        </p>
+        <ul>
+          <li>An item</li>
+          <li>Another item</li>
+          <li>A third item</li>
+          <li>A fourth</li>
+        </ul>
+      </section>
+      <footer>The footer</footer>
+    </article>
+  </main>,
+);
+self.addEventListener("message", ({ data }) => console.log(data));
