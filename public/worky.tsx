fac49c598280017281b6a5d20e7d3bbb3c85d722
Matt Hayes <matt@mysterycommand.com>
2025-04-19 16:06:29 -0500

    refactor: conform to new runtime, test against scanned dom

--- public/worky.tsx
+++ public/worky.tsx
@@ -1,15 +1,25 @@
-import type { Kids } from "./lib/real.ts";
+import type { Child } from "./lib/real.ts";
 
-const Item = (props: { key: string }, kids: Kids) => <li {...props}>{kids}</li>;
+const Item = (_: { key: string }, children: Child[]) => <li>{children}</li>;
 const List = ({ items }: { items: string[] }) => (
   <ul>
     {items.map((i, j) => (
-      <Item key={j + ":" + i.toLocaleLowerCase().replace(/\W/g, "-")}>{i}</Item>
+      <Item key={j + ":" + i.toLocaleLowerCase().replace(/\W/g, "-")}>
+        {i}
+      </Item>
     ))}
   </ul>
 );
-const work = (
+const App = () => (
   <main id="m">
+    <List
+      items={[
+        "An item",
+        "Another item",
+        "A third item",
+        "A fourth",
+      ]}
+    />
     <hr />
     <article className="the-article">
       <header>
@@ -22,22 +32,17 @@ const work = (
           Some <b>more</b>
           <i>content</i>.
         </p>
-        <List
-          items={[
-            "An item",
-            "Another item",
-            "A third item",
-            "A fourth",
-          ]}
-        />
       </section>
       <footer>The footer</footer>
     </article>
   </main>
 );
+const work = <App />;
 
 self.postMessage(work);
 self.addEventListener(
   "message",
-  ({ data }) => console.log(JSON.stringify(data) === JSON.stringify(work)),
+  ({ data }) => {
+    console.log(JSON.stringify(data) === JSON.stringify(work));
+  },
 );
