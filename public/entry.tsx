976e2f987fd2f306573885be92a0a0e16cde2cf8
Matt Hayes <matt@mysterycommand.com>
2025-04-19 07:13:49 -0500

    refactor: move the jsx to the worker

--- public/entry.tsx
+++ public/entry.tsx
@@ -70,30 +70,3 @@ const bild = (thng: Thng<Data> | string): Node => {
     }
   }
 };
-
-console.info(scan(m));
-console.info(bild(
-  <main id="m">
-    <hr />
-    <article className="the-article">
-      <header>
-        <h1>The Title</h1>
-        <h2>A Subheading</h2>
-      </header>
-      <section>
-        <p>Some content.</p>
-        <p>
-          Some <b>more</b>
-          <i>content</i>.
-        </p>
-        <ul>
-          <li>An item</li>
-          <li>Another item</li>
-          <li>A third item</li>
-          <li>A fourth</li>
-        </ul>
-      </section>
-      <footer>The footer</footer>
-    </article>
-  </main>,
-));
--- public/worky.ts
+++
@@ -1,3 +0,0 @@
-console.log("foo");
-self.postMessage("bar");
-self.addEventListener("message", ({ data }) => console.log(data));
