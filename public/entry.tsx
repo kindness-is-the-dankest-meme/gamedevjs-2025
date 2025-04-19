5bb49cb249713df9658c285d3c4b670d5eb89890
Matt Hayes <matt@mysterycommand.com>
2025-04-19 06:59:25 -0500

    refactor: update types, handle falsey children, map 'class' to 'className' during scan

--- public/entry.tsx
+++ public/entry.tsx
@@ -1,19 +1,27 @@
 declare const m: HTMLElement;
 
 type Rndr = keyof (HTMLElementTagNameMap | SVGElementTagNameMap);
-type Thng = {
+type Data = Record<PropertyKey, unknown>;
+type Kid = Thng<Data> | string | false | null | undefined;
+
+type Thng<T extends Data> = {
   rndr: Rndr;
-  data?: { [k: string]: unknown };
-  kids?: Kids;
+  data?: T;
+  kids?: Kid[];
 };
 
-type Kids = (Thng | string)[];
+type Kids = Kid[];
 
 const { from } = Array,
   { assign, fromEntries } = Object;
 
+const nameMap: Record<string, string> = {
+  ["class"]: "className",
+};
+const mapn = (x: string) => nameMap[x] ?? x;
+
 const peek = (as: NamedNodeMap): { [k: string]: unknown } =>
-  fromEntries(from(as, ({ name, value }) => [name, value]));
+  fromEntries(from(as, ({ name, value }) => [mapn(name), value]));
 
 const kids = (acc: Kids, node: ChildNode): Kids => {
   switch (node.nodeType) {
@@ -35,7 +43,7 @@ const kids = (acc: Kids, node: ChildNode): Kids => {
   return acc;
 };
 
-const scan = ({ tagName, attributes, childNodes }: Element): Thng =>
+const scan = ({ tagName, attributes, childNodes }: Element): Thng<Data> =>
   assign(
     { rndr: tagName.toLowerCase() as Rndr },
     attributes.length && { data: peek(attributes) },
@@ -48,11 +56,12 @@ const worky = new Worker("./worky.ts", { type: "module" });
 worky.addEventListener("message", ({ data }) => console.log(data));
 worky.postMessage(scan(m));
 
-const bild = (thng: Thng | string): Node => {
+const bild = (thng: Thng<Data> | string): Node => {
   switch (typeof thng) {
     case "object": {
       const node = assign(document.createElement(thng.rndr), thng.data);
-      thng.kids && thng.kids.forEach((kid) => node.appendChild(bild(kid)));
+      thng.kids &&
+        thng.kids.forEach((kid) => kid && node.appendChild(bild(kid)));
       return node;
     }
 
@@ -62,4 +71,29 @@ const bild = (thng: Thng | string): Node => {
   }
 };
 
-console.info(bild(scan(m)));
+console.info(scan(m));
+console.info(bild(
+  <main id="m">
+    <hr />
+    <article className="the-article">
+      <header>
+        <h1>The Title</h1>
+        <h2>A Subheading</h2>
+      </header>
+      <section>
+        <p>Some content.</p>
+        <p>
+          Some <b>more</b>
+          <i>content</i>.
+        </p>
+        <ul>
+          <li>An item</li>
+          <li>Another item</li>
+          <li>A third item</li>
+          <li>A fourth</li>
+        </ul>
+      </section>
+      <footer>The footer</footer>
+    </article>
+  </main>,
+));
