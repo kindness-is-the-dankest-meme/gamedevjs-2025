a2d57b02e987b60c9ef20aeacee8b8fe5e7dec91
Matt Hayes <matt@mysterycommand.com>
2025-04-19 22:48:00 -0500

    refactor: I think this is going to be it, but now I gotta figure out fragments

--- public/worky.tsx
+++ public/worky.tsx
@@ -1,52 +1,12 @@
-import type { Child } from "./lib/real.ts";
-
-const Item = (_: { key: string }, children: Child[]) => <li>{children}</li>;
-const List = ({ items }: { items: string[] }) => (
-  <ul>
-    {items.map((i, j) => (
-      <Item key={j + ":" + i.toLocaleLowerCase().replace(/\W/g, "-")}>
-        {i}
-      </Item>
-    ))}
-  </ul>
-);
-const P = (_: { key: string }, children: Child[]) => <p>{children}</p>;
 const App = () => (
-  <main id="m">
-    <List
-      items={[
-        "An item",
-        "Another item",
-        "A third item",
-        "A fourth",
-      ]}
-    />
-    <hr />
-    <article className="the-article">
-      <header>
-        <h1>The Title</h1>
-        <h2>A Subheading</h2>
-      </header>
-      <section>
-        {[
-          <P key="foo">Some content.</P>,
-          <P key="bar">
-            Some <b>more</b>
-            <i>content</i>.
-          </P>,
-        ]}
-        <List
-          items={[
-            "An item",
-            "Another item",
-            "A third item",
-            "A fourth",
-          ]}
-        />
-      </section>
-      <footer>The footer</footer>
-    </article>
-  </main>
+  <>
+    <svg></svg>
+    <header></header>
+    <nav></nav>
+    <menu></menu>
+    <aside></aside>
+    <footer></footer>
+  </>
 );
 const work = <App />;
 
