3bdffeb96da3ecb3940d968eed0ce4ded72bd381
Matt Hayes <matt@mysterycommand.com>
2025-04-20 00:35:00 -0500

    refactor: handle fragments and properly build svg tags and attributes

--- public/entry.ts
+++ public/entry.ts
@@ -3,7 +3,7 @@ import type { Child, Tag, Thing } from "./lib/real.ts";
 declare const m: HTMLElement;
 
 const { from } = Array;
-const { assign, fromEntries } = Object;
+const { assign, entries, fromEntries } = Object;
 
 const mapn = (
   (ns: Record<string, string>) => (n: string) => ns[n] ?? n
@@ -51,21 +51,98 @@ const children = (
   return acc;
 };
 
-const scan = ({ tagName, attributes, childNodes }: Element): Thing =>
-  assign(
-    { tag: tagName.toLowerCase() as Tag },
-    attributes.length && { props: peek(attributes) },
-    childNodes.length && {
-      children: childNodes.values().toArray().reduce(children, []),
-    },
-  );
+const scan = (el: Element | NodeListOf<ChildNode>): Thing =>
+  el instanceof Element
+    ? assign(
+      { tag: el.tagName.toLowerCase() as Tag },
+      el.attributes.length && { props: peek(el.attributes) },
+      el.childNodes.length && {
+        children: el.childNodes.values().toArray().reduce(children, []),
+      },
+    )
+    : assign(
+      { tag: null },
+      el.length && { children: el.values().toArray().reduce(children, []) },
+    );
 
 const worky = new Worker("./worky.ts", { type: "module" });
 worky.addEventListener(
   "message",
-  ({ data }) => console.log(JSON.stringify(data) === JSON.stringify(scan(m))),
+  ({ data }) => {
+    console.log(data);
+    const nextNode = bild(data);
+    m.firstChild
+      ? m.replaceChildren(nextNode, m.firstChild)
+      : m.appendChild(nextNode);
+  },
 );
-worky.postMessage(scan(m));
+worky.postMessage(scan(m.childNodes));
+
+const svgs: string[] = [
+  // "a",
+  // "animate",
+  // "animateMotion",
+  // "animateTransform",
+  // "circle",
+  // "clipPath",
+  "defs",
+  // "desc",
+  // "ellipse",
+  // "feBlend",
+  // "feColorMatrix",
+  // "feComponentTransfer",
+  // "feComposite",
+  // "feConvolveMatrix",
+  // "feDiffuseLighting",
+  // "feDisplacementMap",
+  // "feDistantLight",
+  // "feDropShadow",
+  // "feFlood",
+  // "feFuncA",
+  // "feFuncB",
+  // "feFuncG",
+  // "feFuncR",
+  // "feGaussianBlur",
+  // "feImage",
+  // "feMerge",
+  // "feMergeNode",
+  // "feMorphology",
+  // "feOffset",
+  // "fePointLight",
+  // "feSpecularLighting",
+  // "feSpotLight",
+  // "feTile",
+  // "feTurbulence",
+  // "filter",
+  // "foreignObject",
+  "g",
+  // "image",
+  // "line",
+  // "linearGradient",
+  // "marker",
+  // "mask",
+  // "metadata",
+  // "mpath",
+  "path",
+  // "pattern",
+  // "polygon",
+  // "polyline",
+  // "radialGradient",
+  "rect",
+  // "script",
+  // "set",
+  // "stop",
+  // "style",
+  "svg",
+  // "switch",
+  // "symbol",
+  // "text",
+  // "textPath",
+  // "title",
+  // "tspan",
+  "use",
+  // "view",
+] as const;
 
 const cnod = (thing: Thing): Node => {
   switch (typeof thing.tag) {
@@ -78,7 +155,16 @@ const cnod = (thing: Thing): Node => {
     }
 
     case "string": {
-      const node = assign(document.createElement(thing.tag), thing.props);
+      const node = document.createElementNS(
+        svgs.includes(thing.tag)
+          ? "http://www.w3.org/2000/svg"
+          : "http://www.w3.org/1999/xhtml",
+        thing.tag,
+      );
+      thing.props &&
+        entries(thing.props).forEach(([k, v]) =>
+          node.setAttribute(k, String(v))
+        );
       thing.children &&
         thing.children.forEach((t) => t && node.appendChild(bild(t)));
       return node;
