901de21168b6130b82f52823f149a9612ba8a6cd
Matt Hayes <matt@mysterycommand.com>
2025-04-20 22:12:46 -0500

    refactor: generate corner tiles based on size

--- public/components/Pond.tsx
+++ public/components/Pond.tsx
@@ -4,6 +4,20 @@ type PondProps = {
   size: number;
 };
 
+const corners = (s: number) => {
+  const hs = s / 2;
+  return [
+    ["╯i", `M 0 0 H ${hs} A ${hs} ${hs} 0 0 1 0 ${hs} Z`],
+    ["╰i", `M ${s} 0 V ${hs} A ${hs} ${hs} 0 0 1 ${hs} 0 Z`],
+    ["╭i", `M ${s} ${s} H ${hs} A ${hs} ${hs} 0 0 1 ${s} ${hs} Z`],
+    ["╮i", `M 0 ${s} V ${hs} A ${hs} ${hs} 0 0 1 ${hs} ${s} Z`],
+    ["╭o", `M 0 0 V ${s} H ${hs} A ${hs} ${hs} 0 0 1 ${s} ${hs} V 0 Z`],
+    ["╮o", `M 0 0 V ${hs} A ${hs} ${hs} 0 0 1 ${hs} ${s} H ${s} V 0 Z`],
+    ["╯o", `M ${hs} 0 A ${hs} ${hs} 0 0 1 0 ${hs} V ${s} H ${s} V 0 Z`],
+    ["╰o", `M 0 0 V ${s} H ${s} V ${hs} A ${hs} ${hs} 0 0 1 ${hs} 0 Z`],
+  ];
+};
+
 export const Pond = ({ cols, rows, size }: PondProps) => (
   <svg
     width="100%"
@@ -13,63 +27,40 @@ export const Pond = ({ cols, rows, size }: PondProps) => (
     fill="none"
   >
     <defs>
-      <g id="tli">
-        <rect width={size} height={size} />
-        <path d="M 0 0 H 40 A 40 40 0 0 1 0 40 Z" />
-      </g>
-      <g id="tri">
-        <rect width={size} height={size} />
-        <path d="M 80 0 V 40 A 40 40 0 0 1 40 0 Z" />
-      </g>
-      <g id="bri">
-        <rect width={size} height={size} />
-        <path d="M 80 80 H 40 A 40 40 0 0 1 80 40 Z" />
-      </g>
-      <g id="bli">
-        <rect width={size} height={size} />
-        <path d="M 0 80 V 40 A 40 40 0 0 1 40 80 Z" />
-      </g>
-      <g id="bro">
-        <rect width={size} height={size} />
-        <path d="M 0 0 V 80 H 40 A 40 40 0 0 1 80 40 V 0 Z" />
-      </g>
-      <g id="blo">
-        <rect width={size} height={size} />
-        <path d="M 0 0 V 40 A 40 40 0 0 1 40 80 H 80 V 0 Z" />
-      </g>
-      <g id="tlo">
-        <rect width={size} height={size} />
-        <path d="M 40 0 A 40 40 0 0 1 0 40 V 80 H 80 V 0 Z" />
-      </g>
-      <g id="tro">
-        <rect width={size} height={size} />
-        <path d="M 0 0 V 80 H 80 V 40 A 40 40 0 0 1 40 0 Z" />
-      </g>
-      <g id="vi">
+      {corners(size).map(([id, d]) => (
+        <g id={id} key={id}>
+          <rect width={size} height={size} />
+          <path d={d} />
+        </g>
+      ))}
+      <g id="│i">
         <rect width={size} height={size} />
         <rect width={size / 2} height={size} />
       </g>
-      <g id="vo">
+      <g id="│o">
         <rect width={size} height={size} />
         <rect x={size / 2} width={size / 2} height={size} />
       </g>
-      <g id="hi">
+      <g id="─i">
         <rect width={size} height={size} />
         <rect width={size} height={size / 2} />
       </g>
-      <g id="ho">
+      <g id="─o">
         <rect width={size} height={size} />
         <rect y={size / 2} width={size} height={size / 2} />
       </g>
-      <g id="i">
+      <g id="∙i">
         <rect width={size} height={size} />
       </g>
-      <g id="o">
+      <g id="∙o">
         <rect width={size} height={size} />
       </g>
     </defs>
     <g>
-      <use href="#bri" transform="translate(0,0)" />
+      <use href="#╭i" transform="translate(0,0)" />
+      <use href="#╮i" transform="translate(80,0)" />
+      <use href="#╰i" transform="translate(0,80)" />
+      <use href="#╯i" transform="translate(80,80)" />
     </g>
   </svg>
 );
