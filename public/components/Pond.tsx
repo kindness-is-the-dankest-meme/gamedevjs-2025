518db7130f0c18c4e3a3561636a8381d78fb14c9
Matt Hayes <matt@mysterycommand.com>
2025-04-21 11:31:55 -0500

    feat: first pass random grid, need to handle inside / outside now

--- public/components/Pond.tsx
+++ public/components/Pond.tsx
@@ -32,56 +32,128 @@ const tiles = (s: number) => {
  * ╰ ╮ ∙ ∙ ╭ ╯
  * ∙ ╰ ─ ─ ╯ ∙
  */
-const allowedNeighbors = {
+const ns = {
   "╭": {
-    up: "∙─╯╰",
-    right: "─╮╯",
-    down: "│╯╰",
-    left: "∙│╯╮",
+    u: "□─╯╰",
+    r: "─╮╯",
+    d: "│╯╰",
+    l: "□│╯╮",
   },
   "╮": {
-    up: "∙─╯╰",
-    right: "∙│╭╰",
-    down: "│╯╰",
-    left: "╭╰─",
+    u: "□─╯╰",
+    r: "□│╭╰",
+    d: "│╯╰",
+    l: "╭╰─",
   },
   "╯": {
-    up: "│╮╭",
-    right: "∙│╭╰",
-    down: "∙─╮╭",
-    left: "╭╰─",
+    u: "│╮╭",
+    r: "□│╭╰",
+    d: "□─╮╭",
+    l: "╭╰─",
   },
   "╰": {
-    up: "│╮╭",
-    right: "─╮╯",
-    down: "∙─╮╭",
-    left: "∙│╯╮",
+    u: "│╮╭",
+    r: "─╮╯",
+    d: "□─╮╭",
+    l: "□│╯╮",
   },
   "│": {
-    up: "│╮╭",
-    right: "∙│╭╰",
-    down: "│╯╰",
-    left: "∙│╯╮",
+    u: "│╮╭",
+    r: "□│╭╰",
+    d: "│╯╰",
+    l: "□│╯╮",
   },
   "─": {
-    up: "∙─╯╰",
-    right: "─╮╯",
-    down: "∙─╮╭",
-    left: "╭╰─",
+    u: "□─╯╰",
+    r: "─╮╯",
+    d: "□─╮╭",
+    l: "╭╰─",
   },
   "■": {
-    up: "∙─╯╰",
-    right: "∙│╭╰",
-    down: "∙─╮╭",
-    left: "∙│╯╮",
+    u: "■─╯╰",
+    r: "■│╭╰",
+    d: "■─╮╭",
+    l: "■│╯╮",
   },
   "□": {
-    up: "∙─╯╰",
-    right: "∙│╭╰",
-    down: "∙─╮╭",
-    left: "∙│╯╮",
+    u: "□─╯╰",
+    r: "□│╭╰",
+    d: "□─╮╭",
+    l: "□│╯╮",
+  },
+  "": {
+    u: "",
+    r: "",
+    d: "",
+    l: "",
   },
 } as const;
+
+const { from } = Array;
+const { floor, random } = Math;
+const { keys } = Object;
+
+const vlts = keys(ns);
+type Tile = keyof typeof ns;
+const isvt = (t: string): t is Tile => vlts.includes(t);
+
+const outl = (cols: number, rows: number) => (i: number, j: number) =>
+  i === 0 || i === rows - 1 || j === 0 || j === cols - 1 ? "□" : "";
+
+const grid = (cols: number, rows: number, mapt = outl(cols, rows)) =>
+  from(
+    { length: rows },
+    (_, i) => from({ length: cols }, (_, j) => mapt(i, j)),
+  );
+
+const s = (g: string[][]) =>
+  g.map((r) => r.map((c) => c || " ").join("")).join("\n");
+
+const genp = (cols: number, rows: number) =>
+  grid(cols, rows).reduce<string[][]>((
+    acr,
+    r,
+    i,
+    rs,
+  ) => (acr.push(
+    r.reduce<string[]>((acc, c, j) => {
+      if (i === 0 || i === rows - 1 || j === 0 || j === cols - 1) {
+        acc[j] = c;
+        return acc;
+      }
+
+      const [nu, nr, nd, nl] = [
+        acr[i - 1][j],
+        r[j + 1],
+        rs[i + 1][j],
+        acc[j - 1],
+      ];
+
+      if (!(isvt(nu) && isvt(nr) && isvt(nd) && isvt(nl))) {
+        throw new Error("Invalid neighbor");
+      }
+
+      const [au, ar, ad, al] = [
+        ns[nd].u || "",
+        ns[nl].r || "",
+        ns[nu].d || "",
+        ns[nr].l || "",
+      ];
+
+      const as = (au + ar + ad + al).split("").filter((
+        t,
+      ) =>
+        (au === "" || au.includes(t)) && (ar === "" || ar.includes(t)) &&
+        (ad === "" || ad.includes(t)) && (al === "" || al.includes(t))
+      ).join("");
+
+      acc[j] = as.charAt(floor(random() * as.length));
+      return acc;
+    }, []),
+  ),
+    acr), []);
+
+console.log(s(genp(50, 20)));
 
 export const Pond = ({ cols, rows, size }: PondProps) => (
   <svg
