e9d7b4cbc5b538e0c769b0980004958c5e96d442
Matt Hayes <matt@mysterycommand.com>
2025-04-24 08:21:24 -0500

    refactor: make utils more generally useful

--- public/app/utils.ts
+++ public/app/utils.ts
@@ -1,4 +1,5 @@
+export const fmts = <T>(x: T): string => JSON.stringify(x, null, 2);
 export const tapl = <T>(
   x: T,
-  fmt: (x: T) => string = String,
-) => (console.log(fmt(x)), x);
+  fmt?: (x: T) => string,
+): T => (console.log(fmt?.(x) ?? x), x);
