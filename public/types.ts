dc4c2eae3050e8a5df126d16570293c1388627a5
Matt Hayes <matt@mysterycommand.com>
2025-04-25 12:32:03 -0500

    refactor: only expose the types and parsers that get used, send --> rpc (more descriptive)

--- public/types.ts
+++ public/types.ts
@@ -1,6 +1,6 @@
 import { z } from "https://esm.sh/zod@3.24.3";
 
-export const Patch = z.object({
+const Patch = z.object({
   op: z.enum(["add", "remove", "replace"]),
   path: z.array(
     z.union([
@@ -12,7 +12,9 @@ export const Patch = z.object({
   value: z.any(),
 });
 
-export const InnerSize = z
+export type Patch = z.infer<typeof Patch>;
+
+const InnerSize = z
   .object({
     innerWidth: z.number(),
     innerHeight: z.number(),
@@ -22,52 +24,62 @@ export const InnerSize = z
     height,
   }));
 
-export const ResizeEvent = z.object({
+const Rpc = z.object({
+  cbid: z.string(),
+  args: z.tuple([z.any()]),
+});
+
+const ResizeEvent = z.object({
   type: z.literal("resize"),
   target: InnerSize,
 });
-export const SendEvent = z.object({
-  type: z.literal("send"),
-  detail: z.object({
-    callback: z.string(),
-    args: z.tuple([z.any()]),
-  }),
+const RpcEvent = z.object({
+  type: z.literal("rpc"),
+  detail: Rpc,
 });
+
 export const GlobalThisEvent = z.discriminatedUnion("type", [
   ResizeEvent,
-  SendEvent,
+  RpcEvent,
 ]);
 
-export const ErrorEvent = z.object({ type: z.literal("error") });
-export const MessageErrorEvent = z.object({ type: z.literal("messageerror") });
-export const MessageEvent = z.object({
+const ErrorEvent = z.object({ type: z.literal("error") });
+const MessageErrorEvent = z.object({ type: z.literal("messageerror") });
+const MessageEvent = z.object({
   type: z.literal("message"),
   data: Patch,
 });
+
 export const WorkerEvent = z.discriminatedUnion("type", [
   ErrorEvent,
   MessageErrorEvent,
   MessageEvent,
 ]);
 
-export const RenderMsg = z.object({ type: z.literal("render") });
-export const ResizeMsg = z.object({
+const RenderMsg = z.object({ type: z.literal("render") });
+const ResizeMsg = z.object({
   type: z.literal("resize"),
   /**
-   * TODO: figure out how to make this use the output type of InnerSize
+   * TODO: figure out how to make this use the output type of `InnerSize`, what
+   * I'd like to do is something like:
+   *
+   * ```ts
+   * const ResizeMsg = z.output(InnerSize).merge(z.object({
+   *   type: z.literal("resize"),
+   * }));
+   * ```
+   *
    * @see https://zod.dev/?id=zodtype-with-zodeffects
    */
   width: z.number(),
   height: z.number(),
 });
-export const SendMsg = z.object({
-  type: z.literal("send"),
-  callback: z.string(),
-  args: z.tuple([z.any()]),
-});
+const RpcMsg = Rpc.merge(z.object({
+  type: z.literal("rpc"),
+}));
 
 export const Msg = z.discriminatedUnion("type", [
   RenderMsg,
   ResizeMsg,
-  SendMsg,
+  RpcMsg,
 ]);
