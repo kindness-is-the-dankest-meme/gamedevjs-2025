397d1120e6948012ef8e7761900ca73847b80511
Matt Hayes <matt@mysterycommand.com>
2025-04-25 23:23:45 -0500

    style: whitespace

--- public/app/utils.ts
+++ public/app/utils.ts
@@ -3,5 +3,6 @@ export const tapl = <T>(
   x: T,
   fmt?: (x: T) => string,
 ): T => (console.log(fmt?.(x) ?? x), x);
+
 export const amap = (ns: Record<string, string>) => (n: unknown): string =>
   ns[String(n)] ?? String(n);
