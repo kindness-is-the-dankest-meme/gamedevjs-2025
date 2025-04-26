cd88602b2b4356820b09dc7981d5391b05fb8e06
Matt Hayes <matt@mysterycommand.com>
2025-04-25 23:27:14 -0500

    refactor: use the store for pond state

--- public/components/Ponds.tsx
+++ public/components/Ponds.tsx
@@ -1,19 +1,25 @@
+import { selectSize } from "../app/selectors.ts";
+import { useStore } from "../app/store.ts";
+
 type PondsProps = {
   ts: string[][];
-  s: number;
 };
 
-export const Ponds = ({ ts, s }: PondsProps) => (
-  <g>
-    {ts.map((r, i) =>
-      r.map((c, j) => (
-        <use
-          key={`${j}:${i}`}
-          className="tile"
-          href={`#${c || "∙"}`}
-          transform={`translate(${j * s} ${i * s})`}
-        />
-      ))
-    )}
-  </g>
-);
+export const Ponds = ({ ts }: PondsProps) => {
+  const size = useStore(selectSize);
+
+  return (
+    <g>
+      {ts.map((r, i) =>
+        r.map((c, j) => (
+          <use
+            key={`${j}:${i}`}
+            className="tile"
+            href={`#${c || "∙"}`}
+            transform={`translate(${j * size} ${i * size})`}
+          />
+        ))
+      )}
+    </g>
+  );
+};
