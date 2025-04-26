08f03c7af94b397e8267b1a6dba8aba2f2a5c1d6
Matt Hayes <matt@mysterycommand.com>
2025-04-25 23:28:02 -0500

    refactor: use selectors for size, cols and rows

--- public/components/World.tsx
+++ public/components/World.tsx
@@ -1,26 +1,20 @@
-import { type State, useStore } from "../app/store.ts";
 import { grid, tapg } from "../app/grids.ts";
 import { paths } from "../app/paths.ts";
 import { rules } from "../app/rules.ts";
+import { selectCols, selectRows, selectSize } from "../app/selectors.ts";
+import { useStore } from "../app/store.ts";
 import { useMemo } from "../lib/estate.ts";
-import { ceil, floor, π } from "../lib/free.ts";
 import { Boat } from "./Boat.tsx";
 import { Ponds } from "./Ponds.tsx";
 
 const genp = (cols: number, rows: number) =>
   grid(cols, rows).reduce<string[][]>(rules, []);
 
-const selectDims = ({ size, width, height }: State) => ({
-  size,
-  width,
-  height,
-});
-
 export const World = () => {
-  const { size, width, height } = useStore(selectDims);
+  const size = useStore(selectSize);
+  const cols = useStore(selectCols);
+  const rows = useStore(selectRows);
 
-  const cols = useMemo(() => ceil(width / size), [width, size]);
-  const rows = useMemo(() => ceil(height / size), [height, size]);
   const ts = useMemo(() => tapg(genp(cols, rows)), [cols, rows]);
 
   return (
@@ -37,13 +31,8 @@ export const World = () => {
           </g>
         ))}
       </defs>
-      <Ponds ts={ts} s={size} />
-      <Boat
-        x={(cols * size) / 2}
-        y={(rows * size) / 2}
-        r={-π / 2}
-        s={floor(size * 0.8)}
-      />
+      <Ponds ts={ts} />
+      <Boat />
     </svg>
   );
 };
